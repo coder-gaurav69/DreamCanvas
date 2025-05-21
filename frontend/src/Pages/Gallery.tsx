8dea2767b4d51de7c69f737ad1055032e1a8f154
coder-gaurav69 <gauravchand222@gmail.com>
2025-05-21 13:04:05 +0530

    updated

--- frontend/src/Pages/Gallery.tsx
+++ frontend/src/Pages/Gallery.tsx
@@ -126,7 +126,7 @@ const Gallery = () => {
         )}
 
         {authenticated && (images.length>0) && (
-          <div className="grid grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-5">
             {images.map(({ imageUrl,publicId }, index) => (
               <div key={index} className="relative">
                 <img src={imageUrl} alt={`Image ${index}`} />
